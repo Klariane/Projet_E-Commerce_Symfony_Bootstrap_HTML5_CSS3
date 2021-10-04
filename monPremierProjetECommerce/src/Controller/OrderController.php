856b3ca9ec2de4c85839e6a499e9abc22438bd4a
Klariane <eniky2005@yahoo.fr>
2021-10-04 18:13:31 +0200

    mon premier push sur ce projet

--- monPremierProjetECommerce/src/Controller/OrderController.php
+++ monPremierProjetECommerce/src/Controller/OrderController.php
@@ -3,14 +3,25 @@
 namespace App\Controller;
 
 use App\Classe\Cart;
+use App\Entity\Order;
 use App\Form\OrderType;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\OrderDetails;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class OrderController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+
     /**
      * @Route("/commande", name="order")
      */
@@ -55,9 +66,62 @@ class OrderController extends AbstractController
 
         if($form->isSubmitted() && $form->isValid())
         {
+            $date = new \DateTime();
+            $carriers = $form->get('carriers')->getData();
+            $delivery = $form->get('adresses')->getData();
+
+            $delivery_content = $delivery->getFirstname().' '.$delivery->getLastname();
+            $delivery_content .='<br/>' . $delivery->getPhone();
+
+            if($delivery->getCompany())
+            {
+                $delivery_content .='<br/>' . $delivery->getCompany();
+            }
+
+
+            $delivery_content .='<br/>' . $delivery->getAdress();
+            $delivery_content .='<br/>' . $delivery->getPostal();
+            $delivery_content .='<br/>' . $delivery->getCountry().' '. $delivery->getCity();
+
+
+
+
+
+            //enregistrer ma commande Order()
+            $order = new Order();
+            $order ->setUser($this->getUser());
+            $order->setCreatedAt($date);
+            $order->setCarrierName($carriers->getName());
+            $order->setCarrierPrice($carriers->getPrice());
+            $order->setDelivery($delivery_content);
+
+            $order->setIsPaid(0);
+
+            $this->entityManager->persist($order);
+
+
+             //Enregistrer mes produits OrderDetails()
+            foreach($cart->getFull() as $product)
+            {
+                $orderDetails = new OrderDetails();
+                $orderDetails->setMyOrder($order);
+
+                $orderDetails->setProduct($product['product']->getName());
+                $orderDetails->setQuantity($product['quantity']);
+                $orderDetails->setPrice($product['product']->getPrice());
+                $orderDetails->setTotal($product['product']->getPrice() * $product['quantity']);
+                $this->entityManager->persist($orderDetails);
+
+            }
+
+           // $this->entityManager->flush();
+
+
+
+           
             
         }
-        return $this->render('order/index.html.twig',[
+        return $this->render('order/add.html.twig',[
            
             'cart'=>$cart->getFull()
         ]);
